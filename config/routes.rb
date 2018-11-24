57e363b702f36ab0a474c94cddd9dbe5acf49d6e
綿引将司 <watahikimasashi@watahikimasashinoMacBook-Pro.local>
2018-11-24 23:42:13 +0900

    ルーディング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'root/index'
-  root to: 'root#index'
+  get 'blogs/index'
+  root to: 'blogs#index'
   resources :blogs do
     collection do
       post :confirm
